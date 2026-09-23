2b2771330835f3488891e3de2be95738e939db2c
shiy07986-hub <shiy07986@gmail.com>
2026-09-23 10:46:40 +0800

    同步本地照片墙（经 API）

--- photos.js
+++ photos.js
@@ -1,1 +1,1 @@
-window.WALL_PHOTOS = [];
+window.WALL_PHOTOS = ["photos/IMG_2786.jpeg", "photos/IMG_4141.jpeg", "photos/IMG_4332.jpeg", "photos/IMG_4347.jpeg", "photos/IMG_4453.jpeg", "photos/IMG_4533.jpeg", "photos/IMG_4540.jpeg"];
